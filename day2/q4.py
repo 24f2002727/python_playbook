4aeb2d49c64b87790fccc0d6a83d19a6eb283231
24f2002727 <shivamrock811307@gmail.com>
2026-01-17 20:52:01 +0530

    day2 q4 completed

--- day2/q4.py
+++ day2/q4.py
@@ -5,3 +5,13 @@ Write a program that takes a number n and:
 - skips numbers that are divisible by 4,
 - stops the loop completely if a number divisible by 10 is encountered.
 """
+def game(n):
+    for i in range(1,n+1):
+        if i%10==0:
+            break
+        if i %4==0:
+            continue
+        print(i)
+
+n=int(input("Enter the number"))
+game(n)
