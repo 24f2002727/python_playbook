21dd711ddefb9e15307494b306c3fddda1138481
24f2002727 <shivamrock811307@gmail.com>
2026-01-22 16:07:18 +0530

    day5 Q2 completed

--- day5/q2.py
+++ day5/q2.py
@@ -10,19 +10,20 @@ Constraints:
 No loops
 No string conversion
 """
-def reverser(n):
-    if n//10==0:
-        return 1
-    return(n%10*10+is_pallindrome(n//10))
+def count_digits(n):
+    if n == 0:
+        return 0
+    return 1 + count_digits(n // 10)
 
+def reverse_number(n, digits):
+    if n == 0:
+        return 0
+    return (n % 10) * (10 ** (digits - 1)) + reverse_number(n // 10, digits - 1)
 
-def is_pallindrome(n):
-    reverse=reverser(n)
-    print(reverse)
-    if n==reverse:
-        return True
-    return False
-print(is_pallindrome(1))
-print(is_pallindrome(12))
-print(is_pallindrome(121))
-print(is_pallindrome(1234321))
+def is_palindrome(n):
+    digits = count_digits(n)
+    return n == reverse_number(n, digits)
+
+print(is_palindrome(12321))   # True
+print(is_palindrome(123))     # False
+print(is_palindrome(1))       # True
